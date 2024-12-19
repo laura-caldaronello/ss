33db190a6ffa473a7cca3a22446c1b126795427b
laura-caldaronello <caldaronello.l@gmail.com>
2024-12-19 17:09:14 +0100

    backend_spring

--- ss-project/src/environments/environment.prod.ts
+++ ss-project/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   google_login_client_id:
     '788124741625-ii8q63ahojo2uc6o2j4hpalnsrgd25dc.apps.googleusercontent.com',
-  apiUrl: 'http://vps-735af0ca.vps.ovh.net:8030',
+  apiUrl: 'http://backend_spring:8030',
 };
